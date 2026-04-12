ea37197b052b1ced28827a94f34fa736ee4c46b7
croma0724 <croma0724@gmail.com>
2026-04-12 09:34:19 -0400

    Cleanup code, add readme file.

---
+++ Lesson1/Readme.txt
@@ -0,0 +1,10 @@
+Lesson1
+
+Objectives:  display the time
+ time():   		retrieve time elapsed from 1970 in seconds
+ ctime_s():  	convert time in seconds to user friendly format. Sample:
+	 			"Sun Apr 12 09:28:52 2026"
+
+Getting time:
+            time elapsed in seconds   
+   time()   -----------------------> ctime_s() =>  "Sun Apr 12 09:28:52 2026"
--- Lesson11/Lesson11.cpp
+++ Lesson11/Lesson11.cpp
@@ -35,52 +35,7 @@ void JTime::getTimeOffset(int offset)
   stmp = buffer;
 }
 
-
 void JTime::displayCurrentTime()
 {
   std::cout << msg << "now: " << stmp << "\n";
 }
-
-
-#ifdef AAAAAAA
-string getTimeNow()
-{
-  string ret;
-  char buffer[BUFSZ] = { 0 };
-  time_t timestamp;
-
-  // Retrieve current time in seconds.
-  time(&timestamp);
-  // Convert time<sec> to human format
-  ctime_s(buffer, BUFSZ, &timestamp);
-
-  ret = buffer;
-  return ret;
-}
-
-string getTimeOffset(int offset)
-{
-  string ret;
-  char buffer[BUFSZ] = { 0 };
-  time_t timestamp;
-
-  // Retrieve current time in seconds.
-  time(&timestamp);
-  timestamp += offset;
-  // Convert time<sec> to human format
-  ctime_s(buffer, BUFSZ, &timestamp);
-  ret = buffer;
-  return ret;
-}
-
-void displayCurrentTime()
-{
-  string ret = getTimeNow();
-  cout << Msg << "now: " << ret << "\n";
-}
-
-void displayTime(const string& setime)
-{
-  cout << Msg << "offset: " << setime << "\n";
-}
-#endif // AAAAAA
